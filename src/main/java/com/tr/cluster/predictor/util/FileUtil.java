c44d6ce4976da8dc5b751ffe4a70144e952e95db
akemalsaglam <alikemalsaglam90@gmail.com>
2020-04-19 15:22:45 +0300

    refactor.

--- src/main/java/com/tr/cluster/predictor/util/FileUtil.java
+++ src/main/java/com/tr/cluster/predictor/util/FileUtil.java
@@ -6,8 +6,11 @@ import java.net.URLDecoder;
 
 public class FileUtil {
 
+    private static final String TRAINING_DATA_FILE_NAME = "training-data.txt";
+    private static final String SAMPLE_TRAINING_DATA_FILE_NAME = "sample-small-data.txt";
+
     public static String getTrainDataFilePath() throws UnsupportedEncodingException {
-        URL url = FileUtil.class.getClassLoader().getResource("sample-small-data.txt");
+        URL url = FileUtil.class.getClassLoader().getResource(TRAINING_DATA_FILE_NAME);
         return URLDecoder.decode(url.getPath(), "UTF-8");
     }
 }

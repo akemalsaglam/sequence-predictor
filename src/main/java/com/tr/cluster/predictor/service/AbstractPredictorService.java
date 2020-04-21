63efe71fbd19b5739673b1819f3ab5724c572ea1
ezgitetik <ezgitetik92@gmail.com>
2020-04-21 22:14:38 +0300

    add taxi data

--- src/main/java/com/tr/cluster/predictor/service/AbstractPredictorService.java
+++ src/main/java/com/tr/cluster/predictor/service/AbstractPredictorService.java
@@ -3,13 +3,16 @@ package com.tr.cluster.predictor.service;
 import ca.pfv.spmf.algorithms.sequenceprediction.ipredict.database.Item;
 import ca.pfv.spmf.algorithms.sequenceprediction.ipredict.database.Sequence;
 import ca.pfv.spmf.algorithms.sequenceprediction.ipredict.database.SequenceDatabase;
+import ca.pfv.spmf.algorithms.sequenceprediction.ipredict.database.SequenceStatsGenerator;
 import ca.pfv.spmf.algorithms.sequenceprediction.ipredict.predictor.Predictor;
 import ca.pfv.spmf.algorithms.sequenceprediction.ipredict.predictor.TDAG.TDAGPredictor;
 import com.tr.cluster.predictor.util.FileUtil;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Iterator;
 import java.util.List;
 
 public abstract class AbstractPredictorService<T extends Predictor> implements BasePredictorService {
@@ -21,6 +24,20 @@ public abstract class AbstractPredictorService<T extends Predictor> implements B
         SequenceDatabase trainingSet = getTrainingSet();
         Constructor<T> constructor = getConstructor(clazz);
         initializePredictor(tag, optionalParameters, constructor);
+
+        Iterator var4 = trainingSet.getSequences().iterator();
+
+        while(var4.hasNext()) {
+            Sequence sequence = (Sequence)var4.next();
+            System.out.println(sequence.toString());
+        }
+        System.out.println();
+        try {
+            SequenceStatsGenerator.prinStats(trainingSet, " training sequences ");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         this.predictor.Train(trainingSet.getSequences());
     }
 
